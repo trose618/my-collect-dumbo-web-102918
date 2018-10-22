9ff3b1dcedb222c6c92bf41642ae93abeb511623
Terrance Rose Jr <trose618@gmail.com>
2018-10-22 17:52:29 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
-
+def my_collect(collection)
+  
+  index = 0 
+  updated_collection = []
+  
+  while index < collection.length 
+   current = yield collection[index]
+   updated_collection << current
+   index += 1 
+  end
+  updated_collection
+end
