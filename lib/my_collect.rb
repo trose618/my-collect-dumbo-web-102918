8c44614c67042ca10e7ccedb561ba2afab4e0734
Terrance Rose Jr <trose618@gmail.com>
2018-10-22 17:46:45 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,13 @@
 def my_collect(collection)
   
+  index = 0 
+  updated_collection = []
+  
+  while index < collection.length 
+    
+    updated_collection << yield collection[index]
+    index += 1 
+    
+  end
+  updated_collection
 end
